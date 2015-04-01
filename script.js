0fea7da95d8b3342fdbbea636545bcd1e192d8fe
easy-one <mybodya@gmail.com>
2015-04-01 15:46:37 +0200

    small fix

--- script.js
+++ script.js
@@ -48,13 +48,13 @@ function Coupon (options) {
 
             if (claimBtn.buttonType === 'site') {
                 path = "<path id=coupon_icon-share d='M 12 12 v -2 c 0 0 -8 -2 -9 2 c 0 -2 1 -6 9 -6 V 4 l 4 4.021 L 12 12 Z M 9 13 H 1 V 3 h 8 v 1 h 1 V 2 H 0 v 12 h 10 v -2 H 9 V 13 Z'></path>";
-                action = "target='_blank' href='" + claimBtn.actionValue + "' download";
-                onclick[i] = downloadCoupon;
+                action = "target='_blank' href='" + claimBtn.actionValue + "'";
+                onclick[i] = showCoupon;
             }
             else if (claimBtn.buttonType === 'download') {
                 path = "<path id=coupon_icon-inbox-download d='M 8 9.4 l -4 -3.73333 h 2 v -4.66667 h 4 v 4.66667 h 2 L 8 9.4 Z M 13.867 4.73333 H 13 l 1.486 3.73333 H 11 c 0 1.54653 -1.342 2.8 -3 2.8 c -1.656 0 -3 -1.25347 -3 -2.8 H 1.514 L 3 4.73333 H 2.133 L 0 9.4 v 5.6 h 16 v -5.6 L 13.867 4.73333 Z'></path>";
                 action = "target='_blank' href='" + claimBtn.actionValue + "' download";
-                onclick[i] = downloadCoupon;
+                onclick[i] = showCoupon;
             }
             else if (claimBtn.buttonType === 'code') {
                 path = "<path id=coupon_icon-eye d='M 8 3 C 2 3 0 8 0 8 s 2 5 8 5 s 8 -5 8 -5 S 14 3 8 3 Z M 8 12 c -4.366 0 -6.326 -2.938 -6.893 -4 C 1.676 6.935 3.637 4 8 4 c 4.366 0 6.326 2.938 6.893 4 C 14.324 9.065 12.363 12 8 12 Z M 10.121 5.879 C 10.664 6.422 11 7.172 11 8 c 0 1.657 -1.344 3 -3 3 S 5 9.657 5 8 s 1.344 -3 3 -3 v 3 L 10.121 5.879 Z'></path>";
@@ -100,7 +100,7 @@ function Coupon (options) {
 
 
 
-        function downloadCoupon() {
+        function showCoupon() {
             if (doShare) {
                 _("coupon_codeBlock").style.display = 'none';
                 _("coupon_overlay").style.display = 'block';
